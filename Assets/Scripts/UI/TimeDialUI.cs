3954abdb925edae4f2425018759fce5e0f948f9c
Maxwell Sime <maxwellsime@gmail.com>
2024-11-19 21:26:44 +0000

    TimeDialUI update missed in previous commit

---
+++ Assets/Scripts/UI/TimeDialUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class TimeDialUI : MonoBehaviour
+    {
+        private SystemsManager _systemsManager;
+    
+        private void Start()
+        {
+            _systemsManager = GameObject.Find("SystemsManager").GetComponent<SystemsManager>();
+        }
+
+        private void OnMouseDown()
+        {
+            _systemsManager.ForwardTimePeriod();        
+        }
+    }
+}
