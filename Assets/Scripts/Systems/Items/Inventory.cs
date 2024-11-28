2b4ed75b9e4521f3e5a29f6a86a5d603f11413ce
Maxwell Sime <maxwellsime@gmail.com>
2024-11-28 23:57:25 +0000

    Change inventory to array to making it parallel to view

--- Assets/Scripts/Systems/Items/Inventory.cs
+++ Assets/Scripts/Systems/Items/Inventory.cs
@@ -1,61 +1,62 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Inventory;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Systems.Inventory
 {
     public class Inventory : MonoBehaviour, IItemContainer
     {
-        [SerializeField] public List<ItemScriptableObject> items;
+        public ItemScriptableObject[] items = new ItemScriptableObject[30];
 
-        public event Action InventoryChange;
+        public event Action InventoryChange = delegate { };
         
         public void AddItem(ItemScriptableObject item, int quantity = 1)
         {
-            var itemIndex = items.FindIndex(i => i.name == item.name);
-
-            if (itemIndex == -1)
-            {
-                items.Add(item);
-                Debug.Log($"Item {item.name} added to Inventory.");
-            }
-            else
-            {
-                var updatedItem = items[itemIndex];
-                updatedItem.quantity += quantity;
-                items[itemIndex] = updatedItem;
-                Debug.Log($"Item {item.name} added to Inventory with quantity {updatedItem.quantity}.");
-            }
-            
             InventoryChange?.Invoke();
+
+            for (var i = 0; i < items.Length; i++)
+            {
+                if (items[i].id == item.id)
+                {
+                    items[i].quantity += quantity;
+                    Debug.Log($"Item {item.name} added to Inventory with quantity {items[i].quantity}.");
+                    break;
+                }
+                if (items[i] != null) continue;
+                items[i] = item;
+                Debug.Log($"Item {item.name} added to Inventory.");
+                break;
+            }
         }
 
         /* Returns true if item is removed by that quantity
          * Returns false if item cannot be removed by that quantity */
         public bool RemoveItem(ItemScriptableObject item, int quantity = 1)
         {
-            if (!HasItem(item)) return false;
-            var itemToRemove = items.Find(i => i.id == item.id);
-                
-            if (itemToRemove.quantity > quantity)
+            for (var i = 0; i < items.Length; i++)
             {
-                itemToRemove.quantity -= quantity;
-            } 
-            else if (itemToRemove.quantity == quantity)
-            {
-                items.Remove(itemToRemove);
+                if (!EqualityComparer<ItemScriptableObject>.Default.Equals(item, items[i])) continue;
+                if (items[i].quantity > quantity)
+                {
+                    items[i].quantity -= quantity;
+                    return true;
+
+                }
+
+                if (items[i].quantity != quantity) continue;
+                items[i] = null;
+                return true;
             }
-            
-            InventoryChange?.Invoke();
-            
+
             return false;
         }
         
         public void Swap(int indexOne, int indexTwo) =>
             (items[indexOne], items[indexTwo]) = (items[indexTwo], items[indexOne]);
 
-        public bool HasItem(ItemScriptableObject item) => items.Exists(i => i.id == item.id);
+        public bool HasItem(ItemScriptableObject item) => 
+            items.FirstOrDefault(i => i.id == item.id) != null;
     }
 }
