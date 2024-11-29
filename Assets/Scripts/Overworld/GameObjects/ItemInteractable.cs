48a603864fb7d2826e712acb673289fbcc724b4d
Maxwell Sime <maxwellsime@gmail.com>
2024-11-29 23:43:30 +0000

    Ammend NovelUI; Make ItemInteractable one use

--- Assets/Scripts/Overworld/GameObjects/ItemInteractable.cs
+++ Assets/Scripts/Overworld/GameObjects/ItemInteractable.cs
@@ -1,7 +1,7 @@
-using Inventory;
 using Inventory.Controllers;
 using Inventory.Models;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Overworld.GameObjects
 {
@@ -9,15 +9,23 @@ namespace Overworld.GameObjects
     {
         public ItemScriptableObject itemScriptableObject;
         private InventoryController _inventoryController;
+        private BoxCollider2D _collider;
+        private SpriteRenderer _spriteRenderer;
 
         private void Start()
         {
             _inventoryController = GameObject.Find("SystemsManager").GetComponent<InventoryController>();
+            _collider = GetComponent<BoxCollider2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _collider.enabled = true;
+            _spriteRenderer.enabled = true;
         }
 
         private void OnMouseDown()
         {
             _inventoryController.InventoryModel.AddItem(itemScriptableObject);
+            _collider.enabled = false;
+            _spriteRenderer.enabled = false;
         }
     }
 }
